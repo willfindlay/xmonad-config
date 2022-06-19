f5517c629d5431cd8737870a579755f77cd8302a
William Findlay <will@isovalent.com>
2022-06-19 12:41:59 -0400

    build: use cabal
    Signed-off-by: William Findlay <will@isovalent.com>

--- xmonad.hs
+++ xmonad.hs
@@ -4,14 +4,12 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Prompt.ConfirmPrompt
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.SpawnOnce
-import XMonad.Prompt
 import System.Exit
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import qualified DBus as D
 import qualified DBus.Client as D
-import qualified Codec.Binary.UTF8.String as UTF8
 
 import qualified Icons
 
@@ -40,30 +38,30 @@ myWorkspaces = [
 
 -- Startup hook
 myStartupHook = do
-    spawnOnce "/home/will/.config/polybar/launch.sh"
-    spawnOnce "setbg /home/will/.cache/bg"
+    spawnOnce "$HOME/.config/polybar/launch.sh"
+    spawnOnce "setbg $HOME/.cache/bg"
 
 -- Commands to run in keybindings
 dmenuCmd = "dmenu_run -fn 'Meslo LGS:pixelsize=24'"
 
 -- Define custom keybindings
-myKeys conf@XConfig {XMonad.modMask = mod, XMonad.terminal = term} = M.fromList $
+myKeys conf@XConfig {XMonad.modMask = modm, XMonad.terminal = term} = M.fromList $
     [
     -- Kill focused window
-    ((mod .|. shiftMask, xK_q), kill)
+    ((modm .|. shiftMask, xK_q), kill)
     -- Exit out of XMonad
-    , ((mod .|. shiftMask, xK_e), confirmPrompt def "exit" $ io exitSuccess)
+    , ((modm .|. shiftMask, xK_e), confirmPrompt def "exit" $ io exitSuccess)
     -- Reload XMonad config
-    , ((mod .|. shiftMask, xK_r), spawn "xmonad --restart")
+    , ((modm .|. shiftMask, xK_r), spawn "xmonad --restart")
     -- Launch terminal emulator
-    , ((mod, xK_Return), spawn term)
+    , ((modm, xK_Return), spawn term)
     -- Launch dmenu
-    , ((mod,  xK_d), spawn dmenuCmd)
+    , ((modm,  xK_d), spawn dmenuCmd)
     ]
     ++
     -- M-[1..=]   -> Switch workspaces
     -- M-S-[1..=] -> Move window to workspace
-    [((m .|. mod, k), windows $ f i)
+    [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0, xK_minus, xK_equal])
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
@@ -72,17 +70,18 @@ main :: IO()
 main = do
     -- Spawn dbus client
     dbus <- D.connectSession
-    D.requestName dbus (D.busName_ "org.xmonad.Log")
+    _ <- D.requestName dbus (D.busName_ "org.xmonad.Log")
         [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
 
     -- Define config options
     xmonad $ ewmh desktopConfig
         {
         terminal = myTerminal
-        , modMask = mod4Mask
+        , modMask = myMod
         , workspaces = myWorkspaces
         , keys = myKeys
         , logHook = dynamicLogWithPP (dbusLogHook dbus)
+        , startupHook = myStartupHook
         }
 
 -- A log hook for producing dbus events
